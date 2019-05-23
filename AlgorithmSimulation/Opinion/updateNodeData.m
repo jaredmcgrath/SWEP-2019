c427985314e17a498b91a8a42e3b9fe897475992
Apple Math <jaredws35@gmail.com>
2019-05-23 11:07:59 -0400

    Finish Opinion App code review

---
+++ AlgorithmSimulation/Opinion/updateNodeData.m
@@ -0,0 +1,26 @@
+function nodeData = updateNodeData(nodeData, L, timeStep, iteration)
+%% updateNodeData
+% Updates nodeData (rComm, position(s), leftNoise and/or rightNoise)
+% for each iteration
+% If simulation is 1D, each row of nodeData is 
+% [rComm,position,leftNoise,rightNoise]
+% If simulation is 2D, each row of nodeData is
+% [rComm,xPosition,yPosition]
+%
+% Parameters:
+%   nodeData 
+%     Matrix of data for all nodes in format specified above, depending on
+%     simulations dimension(s), before update
+%   L
+%     Laplacian matrix calculated for this iteration
+%   timeStep
+%     Simulated duration per iteration
+%   iteration
+%     The current iteration
+%
+% Returns:
+%   nodeData 
+%     Matrix of data for all nodes in format specified above, depending on
+%     simulations dimension(s), after update
+
+nodeData(:,2) = nodeData(:,2) - timeStep*L*nodeData(:,2);
