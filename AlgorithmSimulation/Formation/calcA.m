ed4b4877e6e7fc518f1c92cb4b51145c4ed5a151
Apple Math <jaredws35@gmail.com>
2019-06-25 09:59:15 -0400

    Revise Formation algorithm to generate dynamic adjacency matrix

---
+++ AlgorithmSimulation/Formation/calcA.m
@@ -0,0 +1,15 @@
+function A = calcA(agentPosition)
+%% calcA
+% Calculates the adjacency matrix, A, from agent state held in agentData
+%
+% Parameters:
+%   agentPosition
+%     n-by-2 vector of (x,y) positions for n agents. This is the 'state'
+%     vector of the agents, q, on which the adjacency matrix is calculated
+%
+% Returns:
+%   A
+%     Adjacency matrix
+
+A = diag(ones(size(agentPosition,1),1));
+A(randi(numel(A),5,1)) = 1;
